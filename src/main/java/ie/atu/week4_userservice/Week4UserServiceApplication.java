f843f058089e99be8030ee33c93af98dbf550dae
JamesA <G00379271@atu.ie>
2023-10-11 14:57:41 +0100

    Create project

---
+++ src/main/java/ie/atu/week4_userservice/Week4UserServiceApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week4_userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week4UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week4UserServiceApplication.class, args);
+    }
+
+}
